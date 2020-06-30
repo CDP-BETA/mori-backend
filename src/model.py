d08bc586d74cbd93084fc3878c4588be60fba92b
Jaeho Cho <jaeho8032@gmail.com>
2020-07-01 01:35:11 +0900

    chore: 대소문자

--- src/model.py
+++ src/model.py
@@ -9,9 +9,9 @@ def get_shap(input, model):
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(input)
     explainer = shap.TreeExplainer(model)
-    cols = ['Pneumonia', 'Hypertension', 'Chest_pain', 'Respiratory_disorder',
-       'Anemia', 'Diabetes', 'Hypoglycemia', 'Fever', 'MACE', 'Abdominal_pain',
-       'Pancreatitis', 'MARRIED', 'MALE']
+    cols = ['pneumonia', 'hypertension', 'chest_pain', 'respiratory_disorder',
+       'anemia', 'diabetes', 'hypoglycemia', 'fever', 'mace', 'abdominal_pain',
+       'pancreatitis', 'married', 'male']
     cols = list(zip(cols, input[0]))
     shap_dict = dict(zip(cols, shap_values[0])) 
     top_keys = sorted(shap_dict, key=lambda dict_key: abs(shap_dict[dict_key]), reverse=True)
