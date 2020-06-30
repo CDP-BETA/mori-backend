e3f1ee09a6618b2b83b682626db053c6a9ee3e8d
Jaeho Cho <jaeho8032@gmail.com>
2020-07-01 02:05:12 +0900

    feat: shap 응답

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import joblib
 from model import get_age, get_shap
 
 app = Flask(__name__)
-CORS(app, resources={r'*': {'origins': ['http://localhost:8080', 'https://memento-mori-zzerjae.endpoint.ainize.ai/']}})
+CORS(app, resources={r'*': {'origins': ['http://localhost:8080', 'https://memento-mori-zzerjae.endpoint.ainize.ai']}})
 
 known_keys = [
     'pneumonia',
@@ -24,17 +24,17 @@ known_keys = [
 ]
 
 desease = {
-    "pneumonia": "폐 관련 질병이",
-    "hypertension": "고혈압이",
-    "chest_pain": "가슴 통증이",
-    "respiratory_disorder": "호흡기 질환이",
-    "anemia": "빈혈이",
-    "diabetes": "당뇨병이",
-    "hypoglycemia": "저혈당이",
-    "fever": "열이",
-    "mace": "심혈관 질환이",
-    "abdominal_pain": "복부 통증이",
-    "pancreatitis": "췌장염이",
+    "pneumonia": "폐 관련 질병이 ",
+    "hypertension": "고혈압이 ",
+    "chest_pain": "가슴 통증이 ",
+    "respiratory_disorder": "호흡기 질환이 ",
+    "anemia": "빈혈이 ",
+    "diabetes": "당뇨병이 ",
+    "hypoglycemia": "저혈당이 ",
+    "fever": "열이 ",
+    "mace": "심혈관 질환이 ",
+    "abdominal_pain": "복부 통증이 ",
+    "pancreatitis": "췌장염이 ",
 }
 
 @app.route('/predict', methods=['POST'])
@@ -52,11 +52,42 @@ def predict():
         req_data.append(answer[key])
 
     pred_age = get_age([req_data], model)
-    shaps = get_shap(req_data, model)
+    shaps = get_shap([req_data], model)
 
-    cause = []    for shap in shaps:
+    causes = []    
+    
+    for shap in shaps:
+        cause = ""
 
-    return {'pred_age': round(pred_age, 1), 'shap': shaps}, 200
+        if shap[0] in desease.keys():
+            if shap[1] == 1 and shap[2] >= 0:
+                continue
+            if shap[1] == 0 and shap[2] <= 0:
+                continue
+
+            cause += desease[shap[0]]
+            if shap[1] == 1:
+                cause += "있어서 "
+            else:
+                cause += "없어서 "
+        elif shap[0] == "married":
+            if shap[1] == 1:
+                cause += "결혼을 해서 "
+            else:
+                cause += "결혼을 하지 않아서 "
+        else:
+            if shap[1] == 1:
+                cause += "성별이 남성이어서 "
+            else:
+                cause += "성별이 여성이어서 "
+        if shap[2] > 0:
+            cause += f"수명이 {round(abs(shap[2]), 1)}살 연장되었습니다."
+        else:
+            cause += f"수명이 {round(abs(shap[2]), 1)}살 단축되었습니다."
+        
+        causes.append(cause)
+
+    return {'age': round(pred_age*100, 1), 'shap': causes}, 200
 
 if __name__ == '__main__':
     model = joblib.load('/app/model.pkl')
--- src/model.py
+++ src/model.py
@@ -1,18 +1,19 @@
 import shap
 import joblib
+import numpy as np
 
-def get_age(input, model):
-    result = model.predict(input)
+def get_age(req_data, model):
+    result = model.predict(req_data)
     return result[0]
 
-def get_shap(input, model):
+def get_shap(req_data, model):
     explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(input)
+    shap_values = explainer.shap_values(np.array(req_data))
     explainer = shap.TreeExplainer(model)
     cols = ['pneumonia', 'hypertension', 'chest_pain', 'respiratory_disorder',
        'anemia', 'diabetes', 'hypoglycemia', 'fever', 'mace', 'abdominal_pain',
        'pancreatitis', 'married', 'male']
-    cols = list(zip(cols, input[0]))
+    cols = list(zip(cols, req_data[0]))
     shap_dict = dict(zip(cols, shap_values[0])) 
     top_keys = sorted(shap_dict, key=lambda dict_key: abs(shap_dict[dict_key]), reverse=True)
     result = []
